5ea32a975dd44c006faf4894021e98c3259c160d
Daniela Cansona <1061779039@fup.edu>
2024-10-03 15:59:46 -0500

    Se crea el archivo AreaTriangulo

---
+++ funcionesProject/AreaTriangulo.py
@@ -0,0 +1,29 @@
+# Ejemplo para calcular el area del triangulo
+
+
+#Variables de entrada
+base = int(input("Ingrese la base: "))
+altura = int(input("Ingrese la altura: "))
+
+#Proceso
+def calcularAreaTriangulo(b,a):
+    area = (b*a)/2
+    return area
+
+#Invocar la funcion
+resultado = calcularAreaTriangulo(base,altura)
+print(f"El area del triangulo cuya base {base}y altura {altura}es: {resultado}")
+
+#funcion con argumentos predeterminados
+def my_function(country = "Colombia"):
+    print("I am from "+country)
+
+my_function()
+my_function("Sweden")
+
+ #Argumentos arbitrarios
+    def mostrarEstudiantes(*args):
+        print("El estudiante: "+args[2])
+
+
+
