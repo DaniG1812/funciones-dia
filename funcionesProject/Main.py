ff382d20942093986b352583b4baf21725045d56
Daniela Cansona <1061779039@fup.edu>
2024-10-03 15:53:30 -0500

    Se crea el archivo main

---
+++ funcionesProject/Main.py
@@ -0,0 +1,11 @@
+#Definicion  de una funcion
+def my_function():
+    print("Hello from a function")
+#Invocar una funcion
+my_function()
+
+def mostrarNombre(nombre):
+    print("Su nombre es "+nombre)
+
+nom= input("Ingrese su nombre")
+mostrarNombre(nom)
